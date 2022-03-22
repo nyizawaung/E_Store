c47daaafae8dd28290d78768f9176d4ab5dc8c17
Nyi Zaw Aung <Nyi Zaw Aung@AGD-OP-PC-1194>
2022-03-22 10:14:24 +0630

    Add project files.

---
+++ EStore/BuinessLayer/EStoreBusinessLayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EStore.BuinessLayer
+{
+    public class EStoreBusinessLayer
+    {
+    }
+}
---
+++ EStore/Model/DBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EStore.Model
+{
+    public class ESDBContext:DbContext
+    {
+        public ESDBContext(DbContextOptions<ESDBContext> options) : base(options){
+
+        }
+    }
+}
